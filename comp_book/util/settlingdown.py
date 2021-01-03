0f1574a6c3b23e1c84a6520ec3d033f538e21cc5
Liam Kelly <liam.james.kelly@gmail.com>
2021-01-03 12:15:50 +0000

    sync

--- comp_book/util/settlingdown.py
+++ comp_book/util/settlingdown.py
@@ -1,12 +1,14 @@
-from scipy.stats import poisson, expon
+from scipy.stats import poisson, expon, randint
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 
 def sample_poisson(sample_size: int) -> None:
-    '''Generates a random sample from the Poisson(10) distribution, and
-    plots the distribution'''
+    '''
+    Generates a random sample from the Poisson(10) distribution, and
+    plots the distribution.
+    '''
 
     fig, ax = plt.subplots(1, 1)
 
@@ -38,8 +40,10 @@ def sample_poisson(sample_size: int) -> None:
 
 
 def sample_exponential(sample_size: int) -> None:
-    '''Generates a random sample from the M(2) distribution, and
-    plots the distribution.'''
+    '''
+    Generates a random sample from the M(2) distribution, and
+    plots the distribution.
+    '''
 
     fig, ax = plt.subplots(1, 1)
 
@@ -53,4 +57,41 @@ def sample_exponential(sample_size: int) -> None:
     ax.legend(loc='best', frameon=False)
     plt.title(("Plot of M(2), " +
                "n=" + str(sample_size)))
+    plt.show()
+
+
+def sample_discrete_uniform(sample_size: int) -> None:
+    '''
+    Generates a random sample from the discrete uniform distribution,
+    with parameters lower=1, upper=12, and plots the distribution.
+
+    We set local variable high to 14 because np.arange output 1 to 13,
+    and randint requires upper to be upper+1.
+    '''
+
+    fig, ax = plt.subplots(1, 1)
+    low, high = 1, 14
+    x = np.arange(randint.ppf(0.01, low, high),
+                  randint.ppf(0.99, low, high))
+    ax.plot(x, randint.pmf(x, low, high), 'bo', ms=8, label='randint pmf')
+    ax.vlines(x, 0, randint.pmf(x, low, high), colors='b', lw=5, alpha=0.5)
+
+    r = randint.rvs(low, high, size=sample_size)
+    df = pd.DataFrame(data=x, columns={'x'})
+
+    a_list = list()
+
+    for i in x:
+        count = 0
+        for j in r:
+            if i == j:
+                count = count+1
+        a_list.append(count/sample_size)
+
+    df = pd.DataFrame(data=x, columns={'x'})
+    df['rel freq'] = a_list
+    ax.bar(x=df['x'], height=df['rel freq'], alpha=0.5)
+    ax.legend(loc='best', frameon=False)
+    plt.title(("Plot of discrete uniform distribution (1, 12)," +
+               " n=" + str(sample_size)))
     plt.show()
