88f370911e11c19e06e92b65607933454c6c45c5
Liam Kelly <liam.james.kelly@gmail.com>
2021-04-12 14:59:00 +0100

    Refactoring of comp books scripts

---
+++ notebooks-py/scripts/data.py
@@ -0,0 +1,83 @@
+
+import pandas as pd
+
+
+class Data():
+    """
+    A class of static methods that returns a specific CSV data file in
+    "/data/" as a pandas DataFrame.
+    """
+
+    PATH: str = "../data/"
+
+    def load(a_csv: str) -> pd.DataFrame:
+        """
+        Returns arg a_csv as a pandas DataFrame. There is no need to
+        state the file type.
+        """
+
+        return pd.read_csv(Data.PATH + a_csv + ".csv")
+
+    def load_tattoos() -> pd.DataFrame:
+        """
+        Returns the data in tattoos.csv as a pandas DataFrame.
+        """
+
+        return Data.load("tattoos")
+
+    def load_workforce() -> pd.DataFrame:
+        """
+        Returns the data in workforce.csv as a pandas DataFrame.
+        """
+
+        return Data.load("workforce")
+
+    def load_membership() -> pd.DataFrame:
+        """
+        Returns the data in membership.csv as a pandas DataFrame.
+        """
+
+        return Data.load("membership")
+
+    def load_response_inhibition() -> pd.DataFrame:
+        """
+        Returns the data in response-inhibition.csv as a pandas
+        DataFrame.
+        """
+
+        return Data.load("response-inhibition")
+
+    def load_distance() -> pd.DataFrame:
+        """
+        Returns the data in distance.csv as a pandas DataFrame.
+        """
+
+        return Data.load("distance")
+
+    def load_coal() -> pd.DataFrame:
+        """
+        Returns the data in coal.csv as a pandas DataFrame.
+        """
+
+        return Data.load("coal")
+
+    def load_plasma() -> pd.DataFrame:
+        """
+        Returns the data in plasma.csv as a pandas DataFrame.
+        """
+
+        return Data.load("plasma")
+
+    def load_goals() -> pd.DataFrame:
+        """
+        Returns the data in goals.csv as a pandas DataFrame.
+        """
+
+        return Data.load("goals")
+
+    def load_epl1819() -> pd.DataFrame:
+        """
+        Returns the data in goals.csv as a pandas DataFrame.
+        """
+
+        return Data.load("epl_1819")
